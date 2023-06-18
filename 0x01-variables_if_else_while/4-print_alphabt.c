51fca02baca38cc9cce007d8830918fe9349ad52
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-18 10:18:40 -0700

    modified 4prinr alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
-* Entry point - main function
-* Description: prints alphabets in lower case except q and e
-* Returns 0 if successful
+* main - Entry point
+* printf - prints description
+* description: prints lowercase alphabets without q and e
+* Return: 0 if successful
 */
 
 int main(void)
 {
 	char lowercase;
-	
+
+
 	for (lowercase = 'a' ; lowercase <= 'z' ; lowercase++)
 	{
 	    if (lowercase != 'q' && lowercase != 'e')
