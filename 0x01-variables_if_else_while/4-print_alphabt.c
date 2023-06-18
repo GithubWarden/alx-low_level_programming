dd8a6dc86f9a6bbe5a3bfaceea2d947d06fdbc9d
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-18 10:29:18 -0700

    more modifications done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* printf - prints description
-* description: prints lowercase alphabets without q and e
-* Return: 0 if successful
-*/
+ * main - Entry point
+ * Description: Prints lowercase alphabets without q and e
+ * Return: 0 if successful
+ */
 
 int main(void)
 {
 	char lowercase;
 
-
-	for (lowercase = 'a' ; lowercase <= 'z' ; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
-	    if (lowercase != 'q' && lowercase != 'e')
-	    {
-	    putchar(lowercase);
-	    }
+		if (lowercase != 'q' && lowercase != 'e')
+		{
+			putchar(lowercase);
+		}
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 }
+
