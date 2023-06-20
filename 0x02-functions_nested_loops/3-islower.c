7d2d85c63bb5c534a417ced97c23d51431326f3e
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-20 12:07:19 -0700

    more tasks done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_lower - prints 1 or 0 depending on input
+ *
+ * Description: checks for lowercase characters
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * isalpha - prints 1 or 0 depending on input
+ *
+ * Description: checks for alphabetic characters
+ *
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{ return (1);
+	}
+
+	else
+	{ return (0);
+	}
+}
