af465879b4d7b9bd80c85ae6d402c88297ba311a
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-20 05:43:33 -0700

    nested loops and functions and headers tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "custom_header.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar using _putchar function
+ *
+ * Return: Always 0 if successful
+ */
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
