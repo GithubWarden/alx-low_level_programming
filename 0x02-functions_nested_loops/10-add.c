04bbb2b18dfb65405e3abb9b366fba7ef5ff9368
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-20 14:18:52 -0700

    modifications tk tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @p: number being added
+ * @q: number being added
+ * Return: Always 0.
+ */
+
+int add(int p, int q)
+{
+	return (p + q);
+}
