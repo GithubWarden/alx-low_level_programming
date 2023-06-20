a17c1be5fc1db3db63ccf8efd81b2e6f3c2892fa
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-20 07:29:27 -0700

    modification

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- * Entry point -  main
- *
+ * Entry point - main
  * Description: prints lowercase alphabets with declared function
- *
  * Result: 0 if successful
  */
-
-void print_alphabet(void) {
-	char lowercase = 'a';
-
-	while (lowercase <= 'z') {
-		putchar(lowercase);
-		lowercase++;
-	}
-
-	putchar('\n');
+int main(void) {
+    print_alphabet();
+    return 0;
 }
 
-int main(void) {
-	print_alphabet();
+void print_alphabet(void) {
+    char lowercase = 'a';
 
-	return 0;
+    while (lowercase <= 'z') {
+        putchar(lowercase);
+        lowercase++;
+    }
+
+    putchar('\n');
 }
 
