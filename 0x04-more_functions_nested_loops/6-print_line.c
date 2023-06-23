740764c83a7a678fd6bba6f56f6783c2e8204c05
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-23 16:42:49 -0700

    modifications

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,30 +11,29 @@ void print_line(int n)
 
 {
 
-        if (n <= 0)
+if (n <= 0)
+	{
 
-        {
+_putchar('\n');
 
-                _putchar('\n');
+}
+else
 
-        } else
+{
 
-        {
-
-                int i;
+int i;
 
 
-                for (i = 1; i <= n; i++)
+for (i = 1; i <= n; i++)
+		{
 
-                {
+_putchar('_');
 
-                        _putchar('_');
+}
 
-                }
+_putchar('\n');
 
-                _putchar('\n');
-
-        }
+}
 
 
 }
