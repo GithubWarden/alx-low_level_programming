80f7fb66a5f5b08f28b07f43fb54336425b390d0
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-22 17:03:35 -0700

    modifications

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -tests function that prints if integer is positove or negative
+ * main -tests function checking positive or negative
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
