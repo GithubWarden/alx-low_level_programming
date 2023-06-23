a5a618c8047570cfdb22efea577b18a8960f02dd
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-23 08:30:47 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - returns upper case characters
+ *
+ * Return: Returns 1 if uppercase and 0 if not
+ */
+
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	
+	else
+	{
+		return (0);
+	}
+}
