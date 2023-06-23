679acb315876953b176619dc143277016454f61f
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-23 15:26:09 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 14 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n;
+	char c;
+
+	n = 0;
+	c = 0;
+
+	while (n < 10)
+	{
+		while (c <= 14)
+		{
+			_putchar(c + '0');
+			c++;
+		}
+
+		_putchar('/n');
+		n++;
+	}
+
+	_putchar('/n');
+
+}
