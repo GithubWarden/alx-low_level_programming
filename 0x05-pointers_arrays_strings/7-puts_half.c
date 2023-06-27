895eb8df8057694ca9c4edd5d54f4c7ff2e52be1
Benjamin Oku <bnjmnoku@gmail.com>
2023-06-27 06:11:10 -0700

    modifications

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -29,9 +29,9 @@ void puts_half(char *str)
 		n = (length_of_the_string - 1) / 2;
 	}
 
-	z = n - 1;
+	z = n;
 
-	while (z < length_of_the_string)
+	while (str[z] != '\0')
 	{
 		putchar(str[z]);
 		z++;
