5ef0a7e44c458d099e9a6522da16d15d98882745
Benjamin Oku <bnjmnoku@gmail.com>
2023-07-03 22:44:31 -0700

    more bettty styled

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,13 +2,9 @@
 
 
 /**
-
  * set_string - sets the value of a pointer to a char
-
  * @s: pointer to pointer
-
  * @to: pointer char
-
  */
 
 
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,15 +3,10 @@
 #include <stdio.h>
 
 /**
-
  * print_diagsums - Entry point
-
  * @a: input
-
  * @size: input
-
  * Return: Always 0 (Success)
-
  */
 
 void print_diagsums(int *a, int size)
