a690c5b3d6af39b6dbacd84b5417982fbf52ef62
Benjamin Oku <bnjmnoku@gmail.com>
2023-07-07 23:44:04 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Description: prints its name followed ny a new line
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
